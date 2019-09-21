8efb0597e9b092fbdb909a0abbc63373ff9dafb4
adam <gog172172@gmail.com>
2019-09-21 21:13:40 +0100

    initial commit

---
+++ src/main/java/com/example/Cloud_Lab/CloudLabApplication.java
@@ -0,0 +1,15 @@
+package com.example.Cloud_Lab;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+
+@EnableConfigurationProperties({FileStorageProperties.class})
+@SpringBootApplication
+public class CloudLabApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CloudLabApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/example/Cloud_Lab/StringObj.java
@@ -0,0 +1,11 @@
+package com.example.Cloud_Lab;
+
+import lombok.Getter;
+import lombok.Setter;
+
+public class StringObj {
+
+    @Getter
+    @Setter
+    private String hello;
+}
