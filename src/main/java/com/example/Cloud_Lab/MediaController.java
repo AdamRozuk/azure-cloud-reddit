c9eaa3bc6c6ab105d97261dc86d029e16a6a742f
adam <gog172172@gmail.com>
2019-10-02 18:42:12 +0100

    add azure storage

--- src/main/java/com/example/Cloud_Lab/MediaController.java
+++ src/main/java/com/example/Cloud_Lab/MediaController.java
@@ -1,15 +1,25 @@
 package com.example.Cloud_Lab;
 
+import com.microsoft.azure.storage.CloudStorageAccount;
+import com.microsoft.azure.storage.StorageException;
+import com.microsoft.azure.storage.blob.CloudBlob;
+import com.microsoft.azure.storage.blob.CloudBlobClient;
+import com.microsoft.azure.storage.blob.CloudBlobContainer;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.io.ByteArrayResource;
 import org.springframework.core.io.Resource;
 import org.springframework.http.*;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
 import javax.servlet.http.HttpServletRequest;
+import java.io.ByteArrayOutputStream;
 import java.io.IOException;
+import java.net.URISyntaxException;
+import java.security.InvalidKeyException;
 import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Collectors;
@@ -17,6 +27,11 @@ import java.util.stream.Collectors;
 @RestController
 @RequestMapping("/media")
 public class MediaController {
+
+    private static final Logger logger = LoggerFactory.getLogger(MediaController.class);
+
+    @Autowired
+    private MediaService fileStorageService;
 
     @GetMapping
     public HttpEntity download() {
@@ -30,21 +45,32 @@ public class MediaController {
         return "Some code";
     }
 
-    private static final Logger logger = LoggerFactory.getLogger(MediaController.class);
-
-    @Autowired
-    private MediaService fileStorageService;
-
     @PostMapping("/uploadFile")
     public UploadFileResponse uploadFile(@RequestParam("file") MultipartFile file) {
-        String fileName = fileStorageService.storeFile(file);
+        String storageConnectionString = "DefaultEndpointsProtocol=https;AccountName=storage1920;AccountKey=Zgo5bykmGxufmew62KYtYGPqV+deZZVdOowEUaz2p1gNm4DmZwruE0afEy/lHyjiQlIvN1M7s7i3iCMhQxN2LQ==;EndpointSuffix=core.windows.net";
+        CloudStorageAccount storageAccount = null;
+        try {
+            storageAccount = CloudStorageAccount.parse(storageConnectionString);
+            CloudBlobClient blobClient = storageAccount.createCloudBlobClient();
+            CloudBlobContainer container = blobClient.getContainerReference("images");
+            // Get reference to blob
+            CloudBlob blob = container.getBlockBlobReference( file.getOriginalFilename());
+            // Upload contents from byte array (check documentation for other alternatives)
+            blob.uploadFromByteArray(file.getBytes(), 0, file.getBytes().length);
+        } catch (URISyntaxException | StorageException | IOException e) {
+            e.printStackTrace();
+        } catch (InvalidKeyException e) {
+            e.printStackTrace();
+        }
+
+        //String fileName = fileStorageService.storeFile(file);
 
         String fileDownloadUri = ServletUriComponentsBuilder.fromCurrentContextPath()
-                .path("/downloadFile/")
-                .path(fileName)
+                .path("/media/downloadFile/")
+                .path(file.getOriginalFilename())
                 .toUriString();
 
-        return new UploadFileResponse(fileName, fileDownloadUri,
+        return new UploadFileResponse(file.getOriginalFilename(), fileDownloadUri,
                 file.getContentType(), file.getSize());
     }
 
@@ -58,25 +84,37 @@ public class MediaController {
 
     @GetMapping("/downloadFile/{fileName:.+}")
     public ResponseEntity<Resource> downloadFile(@PathVariable String fileName, HttpServletRequest request) {
-        // Load file as Resource
-        Resource resource = fileStorageService.loadFileAsResource(fileName);
+        //create client to blob storage
+        String storageConnectionString = "DefaultEndpointsProtocol=https;AccountName=storage1920;AccountKey=Zgo5bykmGxufmew62KYtYGPqV+deZZVdOowEUaz2p1gNm4DmZwruE0afEy/lHyjiQlIvN1M7s7i3iCMhQxN2LQ==;EndpointSuffix=core.windows.net";
+        CloudStorageAccount storageAccount = null;
+        ByteArrayOutputStream byteArrayOutputStream = null;
+        byte[] contents = null;
+        try {
+            storageAccount = CloudStorageAccount.parse(storageConnectionString);
+            CloudBlobClient blobClient = storageAccount.createCloudBlobClient();
+            CloudBlobContainer container = blobClient.getContainerReference("images");
+            // Get reference to blob
+            CloudBlob blob = container.getBlobReferenceFromServer(fileName);
+            byteArrayOutputStream = new ByteArrayOutputStream();
+            blob.download(byteArrayOutputStream);
+            blob.download(byteArrayOutputStream);
+            byteArrayOutputStream.close();
+            contents = byteArrayOutputStream.toByteArray();
+        } catch (URISyntaxException | StorageException | IOException e) {
+            e.printStackTrace();
+            logger.info("probably storage exception !");
+        } catch (InvalidKeyException e) {
+            e.printStackTrace();
+        }
+
+        Resource byteArrayResource = new ByteArrayResource(contents);
 
         // Try to determine file's content type
-        String contentType = null;
-        try {
-            contentType = request.getServletContext().getMimeType(resource.getFile().getAbsolutePath());
-        } catch (IOException ex) {
-            logger.info("Could not determine file type.");
-        }
-
-        // Fallback to the default content type if type could not be determined
-        if(contentType == null) {
-            contentType = "application/octet-stream";
-        }
+        String contentType = "application/octet-stream";
 
         return ResponseEntity.ok()
                 .contentType(MediaType.parseMediaType(contentType))
-                .header(HttpHeaders.CONTENT_DISPOSITION, "attachment; filename=\"" + resource.getFilename() + "\"")
-                .body(resource);
+                .header(HttpHeaders.CONTENT_DISPOSITION, "attachment; filename=\"" + fileName + "\"")
+                .body(byteArrayResource);
     }
 }
--- src/main/java/com/example/Cloud_Lab/MediaService.java
+++ src/main/java/com/example/Cloud_Lab/MediaService.java
@@ -2,6 +2,7 @@ package com.example.Cloud_Lab;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.io.Resource;
+import org.springframework.core.io.ResourceLoader;
 import org.springframework.core.io.UrlResource;
 import org.springframework.stereotype.Service;
 import org.springframework.util.StringUtils;
@@ -31,38 +32,6 @@ public class MediaService {
         }
     }
 
-    public String storeFile(MultipartFile file) {
-        // Normalize file name
-        String fileName = StringUtils.cleanPath(file.getOriginalFilename());
 
-        try {
-            // Check if the file's name contains invalid characters
-            if(fileName.contains("..")) {
-                throw new RuntimeException ("Sorry! Filename contains invalid path sequence " + fileName);
-            }
-
-            // Copy file to the target location (Replacing existing file with the same name)
-            Path targetLocation = this.fileStorageLocation.resolve(fileName);
-            Files.copy(file.getInputStream(), targetLocation, StandardCopyOption.REPLACE_EXISTING);
-
-            return fileName;
-        } catch (IOException ex) {
-            throw new RuntimeException ("Could not store file " + fileName + ". Please try again!", ex);
-        }
-    }
-
-    public Resource loadFileAsResource(String fileName) {
-        try {
-            Path filePath = this.fileStorageLocation.resolve(fileName).normalize();
-            Resource resource = new UrlResource(filePath.toUri());
-            if(resource.exists()) {
-                return resource;
-            } else {
-                throw new RuntimeException("File not found " + fileName);
-            }
-        } catch (MalformedURLException ex) {
-            throw new RuntimeException("File not found " + fileName, ex);
-        }
-    }
 }
 
